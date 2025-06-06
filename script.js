08b9d8e1645ac199804d9662ae09d8a74b7b376d
Treasure <treasure.readme@gmail.com>
2025-06-06 21:13:51 +0100

    Added javascript functionality

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+const { text } = require("framer-motion/client");
+
 const questions=[
     {
-        question:"Which is the largest animal in the world?"
+        question:"Which is the largest animal in the world?",
+        answers:[
+
+        ]
+    }, {
+        question:"Which is the largest animal in the world?",
+        answers:[
+            {text:"Asia",correct:false},
+            {text:"Australia",correct:true},
+            {text:"Artic",correct:false},
+            {text:"Africa",correct:false},
+            
+        ]
     }
 ]
+const questionElement= document.getElementById("question");
+const  answerButton= document.getElementById("answer-buttons");
+const  nextButton= document.getElementById("next-btn");
